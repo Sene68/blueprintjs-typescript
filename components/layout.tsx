0acd17caa9cd0f2391be21919dd517209edeca18
Sene68 <senefull68@gmail.com>
2022-07-25 19:10:32 +0900

    header 제거 및 색상 변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -21,19 +21,19 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <div className='min-h-screen flex flex-col'>
-      <header className='bg-purple-200 sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
+      {/* <header className='bg-purple-200 sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
         Next.js sidebar menu
-      </header>
+      </header> */}
       <div className='flex flex-col md:flex-row flex-1'>
-        <aside className='bg-fuchsia-100 w-full md:w-60'>
+        <aside className='bg-slate-100 w-full md:w-60'>
           <nav>
             <ul>
               {menuItems.map(({ href, title }) => (
                 <li className='m-2' key={title}>
                   <Link href={href}>
                     <a
-                      className={`flex p-2 bg-fuchsia-200 rounded hover:bg-fuchsia-400 cursor-pointer ${
-                        router.asPath === href && 'bg-fuchsia-600 text-white'
+                      className={`flex p-2 bg-slate-200 rounded hover:bg-slate-400 cursor-pointer ${
+                        router.asPath === href && 'bg-slate-600 text-white'
                       }`}
                     >
                       {title}

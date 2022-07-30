76bdd0db35c1cad47b8e1679f36ba6b8891637ba
Sene68 <senefull68@gmail.com>
2022-07-30 15:32:11 +0900

    alignment select 추가

---
+++ components/common/alignmentSelect.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+
+import { Alignment, Button, ButtonGroup } from "@blueprintjs/core";
+
+export interface IAlignSelectProps {
+    align: Alignment | undefined;
+    allowCenter?: boolean;
+    label?: string;
+    onChange: (align: Alignment) => void;
+}
+
+export default function AlignmentSelect(props: IAlignSelectProps) {
+    const label = "Align Text";
+    const allowCenter = true;
+
+    const handleAlignLeft = () => { 
+        props.onChange(Alignment.LEFT);
+    }; 
+
+    const handleAlignCenter = () => {
+        props.onChange(Alignment.CENTER);
+    }
+
+    const handleAlignRight = () => {
+        props.onChange(Alignment.RIGHT);
+    };
+
+    return (
+        <div>
+            {label}
+            <ButtonGroup fill={true} style={{ marginTop: 5 }}>
+                <Button active={props.align === Alignment.LEFT} text="Left" onClick={handleAlignLeft} />
+                {allowCenter && (
+                    <Button
+                        active={props.align == null || props.align === Alignment.CENTER}
+                        text="Center"
+                        onClick={handleAlignCenter}
+                    />
+                )}
+                <Button active={props.align === Alignment.RIGHT} text="Right" onClick={handleAlignRight} />
+            </ButtonGroup>
+        </div>
+    );
+}
